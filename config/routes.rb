0952ec8f515888cd6d19658df52c03dd71c274e3
furiiadoresu <furiiadoresu@gmail.com>
2022-03-08 11:11:15 +0000

    edit routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,12 @@
 Rails.application.routes.draw do
   root to: 'tasks#index'
-  
-  resources :tasks
-  resources :users, only: [:new]
 
   get 'login', to: 'sessions#new'
-  get 'signup', to: 'users#new'
-  
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
 
-
+  get 'signup', to: 'users#new'
   
-  
+  resources :users, only: [:create]
+  resources :tasks
 end
